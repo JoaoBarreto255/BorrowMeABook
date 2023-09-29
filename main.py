4f3a1aa52739c9cdd4d3f9503cf3d1123d832869
Joao Barreto <JoaoBarreto255@users.noreply.github.com>
2023-09-28 22:22:47 -0300

    feat: add get/post in /books endpoint

--- main.py
+++ main.py
@@ -9,13 +9,27 @@ import asyncio
 from fastapi import FastAPI
 from hypercorn.config import Config
 from hypercorn.asyncio import serve
+from model import Repo, Book
 
 app = FastAPI()
+repo = Repo()
 
 @app.get('/hello')
 async def read_hello():
     """Send Hello Message"""
     return {"message": "Hello World!"}
+
+@app.get('/books')
+async def get_all_books():
+    """fetch all books"""
+    return [
+        b.to_dict() for b in repo.get_all_books()
+    ]
+
+@app.post('/books')
+async def create_book(book: Book) -> Book:
+    created = repo.create_book(book)
+    return created
 
 if __name__ == '__main__':
     asyncio.run(serve(app, Config()))
